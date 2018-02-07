8fdff9040f5c199cf392b4d23e4384c6611d99aa
antonpetersson <antonpetersson@users.noreply.github.com>
2018-02-07 12:31:10 +0100

    Fixat uppgiften

--- details.php
+++ details.php
@@ -2,12 +2,17 @@
 $titleEnd = "Details";
 include "header.php";
 
-//print_r( $_GET );
-$selectedPlayer = $_GET["playerList"];
 
-echo "<h3>Allt du behöver veta om ". $_GET["playerList"] . ":</h3><br/>";
+$selectedKey = $_GET['playerList'];
+$selectedPlayer = $listan[$selectedKey];
 
-echo "<h4>Han är " . $listan[$selectedPlayer] . "</h4>";
+
+
+
+
+echo "<h3>Allt du behöver veta om ". $selectedPlayer["name"] . ":</h3><br/>";
+
+echo "<h4>Han är " . $selectedPlayer["details"] . "</h4>";
 dropdownItems($listan);
 
 include "footer.php";
--- function.php
+++ function.php
@@ -1,26 +1,35 @@
 <?php
 krsort($listan, SORT_NUMERIC);
 
-function listItems($listan){
-//skriv ut listan
-foreach ($listan as $key => $value) {
-    echo "$key <br>";
-  }; 
-  echo "<br/>";
+function listItems($listan) {
+  //skriv ut listan
+  foreach($listan as $spelare) {
+      if (isset($spelare["name"])) {
+          echo "<b>".$spelare["name"]."</b>: ".$spelare["points"]." Poäng";
+      }
+      else {
+          die("Något gick fel");
+      }
+      echo "<br/>";
+     
+  }
   echo "<br/>";
 }
 
-function dropdownItems($listan){
-//Skriv ut dropdown
-  echo "<select name='playerList' form='playerForm'>";
-  foreach ($listan as $key => $value) {
-    echo "<option value='".$key."'>".$key."</option>";
-  };
-  echo "</select>";
-  echo "<form method='GET' action='./details.php' id='playerForm'>";
-  echo "<input type='submit' value='Mer info'>";
-  echo "</form>";
-}
+
+ function dropdownItems($listan){
+ //Skriv ut dropdown
+   echo "<select name='playerList' form='playerForm'>";
+
+   foreach ($listan as $key => $spelare) {
+     echo "<option value='" . $key ."'>".$spelare["name"]."</option>";
+   };
+
+   echo "</select>";
+   echo "<form method='GET' action='./details.php' id='playerForm'>";
+   echo "<input type='submit' value='Mer info'>";
+   echo "</form>";
+ }
 
 
 
